b94e322183043afcd4628e16be8c545436d415f4
Henry Nhan <henrynhan1@gmail.com>
2019-01-15 21:48:04 -0600

    added important enums

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/MineralPosition.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+/**
+ * This enumerator allows the robot to detect the position of the mineral.
+ */
+public enum MineralPosition {
+    LEFT, RIGHT, CENTER, NONE
+}

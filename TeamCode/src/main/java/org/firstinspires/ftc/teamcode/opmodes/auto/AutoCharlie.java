4743eeab11bc8f3b3abdb06fbdd8aac55dacc0e4
Henry Nhan <24212468+shadowhen@users.noreply.github.com>
2019-01-15 21:55:40 -0600

    added third main auto program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoCharlie.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * The class is based on the AutoBeta class with the robot being able to land on the ground from
+ * the lander. Unlike AutoBeta, the class implements the landing in the autonomous, making this
+ * class more viable for autonomous period and alliance matching.
+ *
+ * @author Henry
+ * @version 1.0
+ */
+@Autonomous(name = "Auto Charlie", group = "auto")
+public class AutoCharlie extends AutoOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+
+        // No reason for this line but commenting lines below may affect the chances of success
+        robot.setDriveZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // The robot would hold on the lander
+        robot.getLift().getLiftMotor().setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.getLift().getLanderMotor().setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        while (!isStarted()) {
+            telemetry.addData(">", "Press START to start encoder drive forward");
+            telemetry. update();
+        }
+
+        robot.getLift().getLiftMotor().setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        robot.getLift().getLanderMotor().setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        // Lower the robot onto the floor from the lander
+        robot.moveLiftByDistance(0.5, 1000, 5);
+
+        // Moves to the left sideways
+        robot.moveSidewaysByEncoder(0.1, -60, 5);
+        sleep(1000);
+
+        // Runs through the minerals
+        robot.moveByEncoder(DRIVE_SPEED, 1010, 1010, 5);
+        sleep(1000);
+
+        // Deposits the team marker in the depot for autonomous points
+        setSweeperLiftPower(-0.5, 500);
+        setSweeperLiftPower(0.5, 1000);
+
+        // Turns the robot and moves towards the pit for parking points
+        robot.moveByEncoder(TURN_SPEED, -740, 740, 5);
+        sleep(1000);
+        robot.moveByEncoder(DRIVE_SPEED, 1500, 1500, 10);
+        setSweeperLiftPower(-0.5, 500);
+    }
+}

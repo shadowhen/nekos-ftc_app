2aba4efdbf3d0cf9f326aed58e77c35c878a74d4
Henry Nhan <henrynhan1@gmail.com>
2019-01-18 23:19:14 -0600

    fixed slider and dumper controls and adjusted constant speed values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriveOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriveOp.java
@@ -17,6 +17,7 @@ import org.firstinspires.ftc.teamcode.robot.Dumper;
 @TeleOp(name = "Drive Op 1.2 - Competition", group = "drive")
 public class DriveOp extends OpMode {
 
+    // Constant drive speed  for buttons
     private static final double DRIVE_SPEED = 0.5;
     private static final double DRIVE_SIDEWAYS_SPEED = 0.8;
     private static final double TURN_SPEED = 0.8;
@@ -29,7 +30,9 @@ public class DriveOp extends OpMode {
     public static final double SIDEWAYS_RF_SPEED = 0.8;
     public static final double SIDEWAYS_RR_SPEED = 0.8;
 
+    // Constant other speed for other robot's mechanisms
     private static final double LIFT_POWER = 0.5;
+    private static final double SLIDER_POWER = 0.15;
     private static final double SWEEPER_POWER = 0.5;
     private static final double SWEEPER_LIFT_POWER = 0.45;
 
@@ -39,19 +42,21 @@ public class DriveOp extends OpMode {
     // Position of the dumper's eervo
     private double dumperPosition = 0.5;
 
-    private boolean sidewaysState = false;
-    private boolean sidewaysStateButtonDown;
+    private boolean sidewaysControlState = false;
+    private boolean sidewaysControlStateButtonDown;
 
     private DriveBot robot;
 
     @Override
     public void init() {
+        // Initializes the robot for driver control
         robot = new DriveBot();
         robot.init(hardwareMap, telemetry);
 
         // Drive motors will use encoders for setting the speed value
         robot.setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        // Prevent the lift from moving
         robot.getLift().setLiftPower(0.0);
 
         // Gets the current position of the server dumper, so the dumper position can match with
@@ -65,25 +70,28 @@ public class DriveOp extends OpMode {
         // Tells the user that the robot is ready to start
         telemetry.addData(">", "waiting for START...");
         telemetry.addData("sweeper lift power", robot.getLift().getLiftMotor().getPower());
-        telemetry.addData("position", robot.getDumper().getServoDumper().getPosition());
+        telemetry.addData("dumper position", robot.getDumper().getServoDumper().getPosition());
+        telemetry.addData("cr servo power", robot.getSweeper().getSweeperPower());
         telemetry.update();
     }
 
     @Override
     public void start() {
+        // Allow the robot's lift and latch to work properly
         robot.getSweeper().getLiftMotor().setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         robot.getLift().getLanderMotor().setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     @Override
     public void loop() {
+        // These methods represent certain functions on the robot
         drive();
         lift();
         dump();
         sweep();
 
         // Sends the info to the driver station for debugging and important acoustics
-        telemetry.addData("Sideways Motion Controls", sidewaysState ? "Dpad" : "Bumpers");
+        telemetry.addData("Sideways Motion Controls", sidewaysControlState ? "D-Pad" : "Bumpers");
         telemetry.addLine("-------------------");
         telemetry.addData("Left Motor-Front Power", "%.2f", robot.getMotorDriveLeftFront().getPower());
         telemetry.addData("Left Motor-Rear Power", "%.2f", robot.getMotorDriveLeftRear().getPower());
@@ -109,17 +117,17 @@ public class DriveOp extends OpMode {
         float joystickRightYOne = gamepad1.right_stick_y;
 
         // Changes the control layout for turning and sideways motion on bumpers and dpad
-        if (!sidewaysStateButtonDown && gamepad1.a) {
-            sidewaysState = !sidewaysState;
-            sidewaysStateButtonDown = true;
-        } else if (sidewaysStateButtonDown && !gamepad1.a) {
-            sidewaysStateButtonDown = false;
+        if (!sidewaysControlStateButtonDown && gamepad1.a) {
+            sidewaysControlState = !sidewaysControlState;
+            sidewaysControlStateButtonDown = true;
+        } else if (sidewaysControlStateButtonDown && !gamepad1.a) {
+            sidewaysControlStateButtonDown = false;
         }
 
         // Set the drive power based on the input of gamepad 1
         if (gamepad1.left_bumper) {
-            // Move the robot sideways left or turn left using the left bumper
-            if (!sidewaysState) {
+            // Move the robot sideways left or turn left depending the sideways state
+            if (!sidewaysControlState) {
                 robot.getMotorDriveLeftFront().setPower(-SIDEWAYS_LF_SPEED);
                 robot.getMotorDriveLeftRear().setPower(SIDEWAYS_LR_SPEED);
                 robot.getMotorDriveRightFront().setPower(SIDEWAYS_RF_SPEED);
@@ -131,7 +139,7 @@ public class DriveOp extends OpMode {
 
         } else if (gamepad1.right_bumper) {
             // Move the robot sideways right or turn right using the right bumper
-            if (!sidewaysState) {
+            if (!sidewaysControlState) {
                 robot.getMotorDriveLeftFront().setPower(SIDEWAYS_LF_SPEED);
                 robot.getMotorDriveLeftRear().setPower(-SIDEWAYS_LR_SPEED);
                 robot.getMotorDriveRightFront().setPower(-SIDEWAYS_RF_SPEED);
@@ -142,16 +150,16 @@ public class DriveOp extends OpMode {
             }
 
         } else if (gamepad1.dpad_up) {
-            // Moves the robot forwards using dpad up
+            // Moves the robot forwards
             robot.setDrivePower(-DRIVE_SPEED, -DRIVE_SPEED);
 
         } else if (gamepad1.dpad_down) {
-            // Moves the robot backwards using dpad down
+            // Moves the robot backwards
             robot.setDrivePower(DRIVE_SPEED, DRIVE_SPEED);
 
-        } else if (gamepad1.dpad_left) {
+        } else if (gamepad1.dpad_right) {
             // Moves the robots sideways left or turn left using left dpad
-            if (sidewaysState) {
+            if (sidewaysControlState) {
                 robot.getMotorDriveLeftFront().setPower(-SIDEWAYS_LF_SPEED);
                 robot.getMotorDriveLeftRear().setPower(SIDEWAYS_LR_SPEED);
                 robot.getMotorDriveRightFront().setPower(SIDEWAYS_RF_SPEED);
@@ -160,9 +168,9 @@ public class DriveOp extends OpMode {
                 robot.setDrivePowerTurn(-TURN_SPEED, -TURN_SPEED);
             }
 
-        } else if (gamepad1.dpad_right) {
+        } else if (gamepad1.dpad_left) {
             // Moves the robots sideways right or turn right using right dpad
-            if (sidewaysState) {
+            if (sidewaysControlState) {
                 robot.getMotorDriveLeftFront().setPower(SIDEWAYS_LF_SPEED);
                 robot.getMotorDriveLeftRear().setPower(-SIDEWAYS_LR_SPEED);
                 robot.getMotorDriveRightFront().setPower(-SIDEWAYS_RF_SPEED);
@@ -183,14 +191,15 @@ public class DriveOp extends OpMode {
      * Controls the lift
      */
     private void lift() {
+        // TODO: Fix these references. They are actually the slider now.
         // Raises or lowers the lift of the robot using lift motor attached to the lift
         if (gamepad2.a) {
             // Lowers the lift
-            robot.getLift().setLiftPower(-LIFT_POWER);
+            robot.getLift().setLiftPower(-SLIDER_POWER);
 
         } else if (gamepad2.y) {
             // Raises the lift
-            robot.getLift().setLiftPower(LIFT_POWER);
+            robot.getLift().setLiftPower(SLIDER_POWER);
 
         } else {
             // Zeros the power of the lift if the driver does not press the lift button
@@ -218,9 +227,9 @@ public class DriveOp extends OpMode {
         float totalTrigger = leftTrigger - rightTrigger;
 
         // Increments or decrements the dumper's servo position
-        if (totalTrigger < -0.1f) {
+        if (totalTrigger > 0.1f) {
             dumperPosition = dumperPosition + DUMPER_SPEED;
-        } else if (totalTrigger > 0.1f) {
+        } else if (totalTrigger < -0.1f) {
             dumperPosition = dumperPosition - DUMPER_SPEED;
         }
         dumperPosition = Range.clip(dumperPosition, Dumper.MIN_POSITION, Dumper.MAX_POSITION);

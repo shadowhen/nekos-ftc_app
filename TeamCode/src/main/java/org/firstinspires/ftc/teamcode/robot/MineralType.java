44d4b6b9e90acc686dbfadb4dd26c21c6af5be74
Henry Nhan <henrynhan1@gmail.com>
2019-02-19 16:27:05 -0600

    updated helper classes for detectors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/MineralType.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/MineralType.java
@@ -1,5 +1,10 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+/**
+ * This enum allows for mineral types which relate to the minerals on the field.
+ * @author Henry
+ * @version 1.0
+ */
 public enum MineralType {
 
     GOLD, SILVER, NONE;

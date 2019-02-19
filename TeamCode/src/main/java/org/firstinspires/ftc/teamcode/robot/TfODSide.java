6b889b11a0fb07795af56e77e0b0f35b67578a38
Henry Nhan <24212468+shadowhen@users.noreply.github.com>
2019-02-19 16:27:05 -0600

    updated helper classes for detectors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TfODSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TfODSide.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.robot;
 /**
  * This enumerator offers Tensorflow Object Detection Side, so the camera can use a side to
  * determine the position of the minerals.
+ * @author Henry
+ * @version 1.0
  */
 public enum TfODSide {
 

0cdff46b084260898f02d63cfb167245a8479dde
Henry Nhan <henrynhan1@gmail.com>
2019-02-21 17:05:31 -0600

    disabled op programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/examples/RevHubImuOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/examples/RevHubImuOp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes.examples;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -17,6 +18,7 @@ import org.firstinspires.ftc.teamcode.robot.AutoBot;
  * @version 1.0
  */
 @TeleOp(name = "REV Hub IMU", group = "test")
+@Disabled
 public class RevHubImuOp extends LinearOpMode {
 
     private static final double TURN_SPEED = 0.25;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/examples/TensorFlowDetectorOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/examples/TensorFlowDetectorOp.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.examples;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -17,6 +18,7 @@ import java.util.List;
  * @version 0.1
  */
 @TeleOp(name = "Tensor Flow Detector - DEV")
+@Disabled
 public class TensorFlowDetectorOp extends LinearOpMode {
 
     private VuforiaDetector vuforiaDetector;

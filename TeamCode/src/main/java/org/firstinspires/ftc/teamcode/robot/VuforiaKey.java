474aeac5284f5c082d8686a5f4fd135d7494acca
Henry Nhan <henrynhan1@gmail.com>
2019-04-22 14:45:24 -0500

    removed the key

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/VuforiaKey.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/VuforiaKey.java
@@ -8,5 +8,5 @@ package org.firstinspires.ftc.teamcode.robot;
 public final class VuforiaKey {
 
     // This key is required to use vuforia
-    public static final String VUFORIA_KEY = "AZMPX1H/////AAABmaIRlZdjX0BopFH1S9hDBg82mx5pFmtsecUNTU5aCHuwhpVU63HiO0bQdqg/Lnm/jS8c94vsKvSJbckJEYKUHUz8omD9dYtmyUKCUuHmAUk8Z/wQmKayAIg4YDUv82uXwQQfFrs7abGSjTxcJzXyTvNYI8mlIKjw/qdePWJFEgrXpEzwjZMlPhwCLQDQPBegHFU7d27PbGgll00fsIf+/27FYLVE2r4TKH3zFyzdXJrXSiy/Qx9pOx2FCTxC9DPFZTx3qdcAiF8dS2ZcWVUD8ciG/GC7k3KDoJtPvco08+bcaNbpQPlJtYGy7hflnFATVaPF0PGFcEWFZl2RH876bDaYIwZFtnjD1a9BUxjp4iu1";
+    public static final String VUFORIA_KEY = "-- YOUR KEY --";
 }
